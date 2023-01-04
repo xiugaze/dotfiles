0d5e84dcea4054525f540be13c02d42a4a0d2d83
Caleb Andreano <calebandreano@gmail.com>
2023-01-04 17:03:11 -0600

    vimtex

--- dwm/config.h
+++ dwm/config.h
@@ -63,7 +63,7 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_s,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_t, spawn,          {.v = termcmd } },
+	{ MODKEY | ShiftMask,                       XK_t, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
