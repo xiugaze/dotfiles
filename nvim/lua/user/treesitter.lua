6d8229f794dbf424f4e02505029904434ab88c9a
xiugaze <calebandreano@gmail.com>
2023-01-16 18:31:29 -0600

    zathura

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -8,7 +8,7 @@ configs.setup {
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
+    disable = { "latex", }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { "yaml" } },
