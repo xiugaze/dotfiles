fb3637f42ccf013b52745dcfbe391ce492ca9c0a
xiugaze <calebandreano@gmail.com>
2023-06-08 13:25:24 -0500

    dap

---
+++ nvim/lua/core/dap.lua
@@ -0,0 +1,19 @@
+require("dapui").setup()
+
+local dap, dapui = require("dap"), require("dapui")
+
+
+-- close and open UI automatically
+dap.listeners.after.event_initialized["dapui_config"] = function ()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function ()
+  dapui.close()
+end
+dap.listeners.after.event_exited["dapui_config"] = function ()
+  dapui.close()
+end
+
+vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+vim.keymap.set("n", "<Leader>ds", ":DapStepOver<CR>")
