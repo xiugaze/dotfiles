cf18cd6fc316bccefa47a9d1e00bd03a486cd99e
Caleb Andreano <calebandreano@gmail.com>
2024-04-04 19:52:14 -0500

    obs

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -2,14 +2,14 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
+--  ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  event = {
+    -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    "BufReadPre ~/docs/school-notes/**.md",
+    "BufNewFile ~/docs/school-notes/**.md",
+  },
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
@@ -23,6 +23,7 @@ return {
         path = "~/docs/school-notes",
       },
     },
+  disable_frontmatter = true
 
     -- see below for full list of options 👇
   },
