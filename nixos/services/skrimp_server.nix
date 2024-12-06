ca25abb9a1a647f2ec7bb40e2d1f030c942e0cea
Caleb Andreano <calebandreano@gmail.com>
2024-12-06 15:07:08 -0600

    nix: add chapterhouse configuration

---
+++ nixos/services/skrimp_server.nix
@@ -0,0 +1,68 @@
+ {config, pkgs, lib, ...}:
+ 
+ let
+   cfg = config.services.skrimp_server;
+ in
+ 
+ with lib;
+ 
+ {
+   options = {
+     services.skrimp_server = {
+       enable = mkOption {
+         default = false;
+         type = with types; bool;
+         description = ''
+           Start server
+         '';
+       };
+ 
+       user = mkOption {
+         default = "username";
+         type = with types; uniq string;
+         description = ''
+           Name of the user.
+         '';
+       };
+     };
+   };
+ 
+   config = mkIf cfg.enable {
+     systemd.services.skrimp_server = {
+       wantedBy = [ "default.target" ]; 
+       after = [ "network.target" ];
+       description = "Start the skrimp shack";
+       serviceConfig = {
+         Type = "simple";
+         User = "${cfg.user}";
+         WorkingDirectory = /home/server/minecraft/skrimp_server;
+         ExecStart = 
+             let start = pkgs.writeShellApplication {
+                 name = "skrimp-server-start";
+                 runtimeInputs = with pkgs; [ jdk21_headless ];
+                 text = ''
+                   cd /home/server/minecraft/skrimp_server
+                   java -Xms12G -Xmx12G -jar server.jar nogui
+                 '';
+               };
+              in 
+                lib.getExe start;
+         ExecStop = 
+             let stop = pkgs.writeShellApplication {
+               name = "skrimp-server-stop";
+               runtimeInputs = with pkgs; [ mcrcon ];
+               text = ''
+                  mcrcon -H localhost -P 25575 -p "popo" stop
+
+                  while kill -0 "$MAINPID" 2>/dev/null
+                  do
+                    sleep 0.5
+                  done
+               '';
+             };
+           in 
+            lib.getExe stop;
+       };
+     };
+   };
+ }
